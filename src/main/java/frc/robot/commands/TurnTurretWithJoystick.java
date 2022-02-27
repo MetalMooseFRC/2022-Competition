74ee8a33c8cf5f1864cf8cdf91a30b37834ec90c
TLarry <larry.dech@westtown.edu>
2022-02-27 11:51:50 -0500

    clamp turret

--- src/main/java/frc/robot/commands/TurnTurretWithJoystick.java
+++ src/main/java/frc/robot/commands/TurnTurretWithJoystick.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
-
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Turret;
 
@@ -27,7 +27,9 @@ public class TurnTurretWithJoystick extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_turret.m_turretMotor.set(MathUtil.clamp(m_speedSupplier.getAsDouble(),-0.3,0.3));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
