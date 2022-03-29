360a1f8c55044829ff4685526dd2e617d34575f8
TLarry <larry.dech@westtown.edu>
2022-03-29 15:17:03 -0400

    PullUp Step2

--- src/main/java/frc/robot/commands/PullUpStep2.java
+++ src/main/java/frc/robot/commands/PullUpStep2.java
@@ -5,32 +5,60 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.Hanger;
+import static frc.robot.Constants.Hanger.*;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class PullUpStep2 extends PIDCommand {
-  /** Creates a new PullUpStep2. */
-  public PullUpStep2() {
+
+  private Hanger m_hanger;
+  private double m_currentPosition;
+  
+
+  /** Creates a new RaiseHangerToHeight. */
+  public PullUpStep2(Hanger hanger) {
     super(
         // The controller that the command will use
-        new PIDController(0, 0, 0),
+        new PIDController(HANGER_PULL_kP, HANGER_PULL_kI, HANGER_PULL_kD),
         // This should return the measurement
-        () -> 0,
+        hanger::getHangerPosition,
         // This should return the setpoint (can also be a constant)
-        () -> 0,
+        STEP_2 ,
         // This uses the output
         output -> {
-          // Use the output here
+      
+          hanger.set(output + 0.04);
+        
         });
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(hanger);
+
     // Configure additional PID options by calling `getController` here.
+    m_hanger = hanger;
+    
+
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    m_currentPosition = m_hanger.getHangerPosition();
+    
+    if(m_currentPosition > STEP_2){
+      m_controller.close();
+      m_hanger.set(0.06);   //hold
+      return true;
+    }
+      
+          
+    return false;//m_currentPosition > MAX_HEIGHT - HANGER_POSITION_TOLERANCE;
   }
+
+  
+  
 }
